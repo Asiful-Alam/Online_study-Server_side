e1af4dc6bcfc6fce19f4cb29eb61bbaf6cc0c9be
OgAsif <ASIF>
2024-05-11 03:23:05 +0600

    added basic setup

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(express.json());
 // zW9qgr8LJjJcYS1v
 
 // online-study
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
 
 
 
@@ -38,12 +36,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+   
+    // await client.close();
   }
 }
 run().catch(console.dir);
