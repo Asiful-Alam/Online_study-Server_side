369197556a2fa568e53d3bd9e163586692359492
OgAsif <ASIF>
2024-05-14 15:57:12 +0600

    add jwt token and get cookies and delete

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:[ 'http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 // MongoDB URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uqgpfrz.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,9 +33,10 @@ async function run() {
   try {
     await client.connect();
 
-    const assignmentCollection = client.db("assignmentDB").collection("assignment");
+    const assignmentCollection = client
+      .db("assignmentDB")
+      .collection("assignment");
     const mylistCollection = client.db("mylistDB").collection("mylist");
-   
 
     // Routes
     // Add assignment
@@ -39,32 +46,30 @@ async function run() {
       const result = await assignmentCollection.insertOne(AddformData);
       res.send(result);
     });
-    
-    
 
     // Get all assignment
     app.get("/assignment", async (req, res) => {
       const cursor = assignmentCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+      console.log('cookies',req.cookies);
     });
 
-//  delete assignment
+    //  delete assignment
     app.delete("/assignment/:id", async (req, res) => {
-      const id=req.params.id;
-      const query={_id: new ObjectId(id) }
-      const result =await assignmentCollection.deleteOne(query);
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentCollection.deleteOne(query);
       res.send(result);
     });
-  //  update assignment
+    //  update assignment
 
-    app.get ('/assignment/:id', async (req, res) =>{
+    app.get("/assignment/:id", async (req, res) => {
       const id = req.params.id;
-      const query ={_id: new ObjectId(id)};
-      const result = await assignmentCollection.findOne(query); 
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentCollection.findOne(query);
       res.send(result);
-    })
-
+    });
 
     // put for updt
     app.put("/assignment/:id", async (req, res) => {
@@ -77,24 +82,22 @@ async function run() {
       const assignment = {
         $set: {
           title: updatedItem.title || currrrLoc.title,
-          description:
-            updatedItem.description || currrrLoc.description,
-            marks: updatedItem.marks || currrrLoc.countrmarksy_name,
-            thumbnailUrl: updatedItem.thumbnailUrl || currrrLoc.thumbnailUrl,
-            difficultyLevel:
+          description: updatedItem.description || currrrLoc.description,
+          marks: updatedItem.marks || currrrLoc.countrmarksy_name,
+          thumbnailUrl: updatedItem.thumbnailUrl || currrrLoc.thumbnailUrl,
+          difficultyLevel:
             updatedItem.difficultyLevel || currrrLoc.difficultyLevel,
-            dueDate: updatedItem.dueDate || currrrLoc.dueDate,
-          
+          dueDate: updatedItem.dueDate || currrrLoc.dueDate,
         },
       };
       console.log("db daata", currrrLoc);
       const result = await assignmentCollection.updateOne(filter, assignment);
       res.send(result);
     });
-   
-  //  mylist
 
-// Get assignment by email
+    //  mylist
+
+    // Get assignment by email
     // app.get("/mylist/:email", async (req, res) => {
     //   try {
     //     const email = req.params.email;
@@ -117,78 +120,107 @@ async function run() {
     // });
     // Inside your backend code
 
-// Add assignment to mylist
-app.post("/mylist", async (req, res) => {
-  try {
-    const { email, data, conclusion,assignment_id } = req.body;
-    const result = await mylistCollection.insertOne({ email, data, conclusion,assignment_id });
-    const results = await assignmentCollection.updateOne(
-      { _id: ObjectId.createFromHexString(assignment_id) },
-      { $set: {type: "PENDING"} }  
-  );
-    res.json({ inserted: result.insertedCount });
-  } catch (error) {
-    console.error("Error adding to mylist:", error);
-    res.status(500).json({ error: "Failed to add to mylist" });
-  }
-});
+    // Add assignment to mylist
+    app.post("/mylist", async (req, res) => {
+      try {
+        const { email, data, conclusion, assignment_id } = req.body;
+        const result = await mylistCollection.insertOne({
+          email,
+          data,
+          conclusion,
+          assignment_id,
+        });
+        
+        const results = await assignmentCollection.updateOne(
+          { _id: ObjectId.createFromHexString(assignment_id) },
+          { $set: { type: "PENDING" } }
+        );
+        res.json({ inserted: result.insertedCount });
+        console.log('cookies',req.cookies);
+      } catch (error) {
+        console.error("Error adding to mylist:", error);
+        res.status(500).json({ error: "Failed to add to mylist" });
+      }
+    });
+
+    app.get("/mylist/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const results = await mylistCollection.find(query).toArray();
+      const assignmentList = results?.map((result) =>
+        ObjectId.createFromHexString(result.assignment_id.toString())
+      );
+      const assignments = await assignmentCollection
+        .find({ _id: { $in: assignmentList } })
+        .toArray();
+      res.send(assignments);
+    });
+
+    // add all list
+    app.get("/all-list", async (req, res) => {
+      const results = await assignmentCollection.find().toArray();
+      res.send(results);
+    });
+
+    // give mark condition
+    app.put("/give-mark", async (req, res) => {
+      const { assignment_id, given_mark } = req.body;
+      const result = await assignmentCollection.updateOne(
+        { _id: ObjectId.createFromHexString(assignment_id) },
+        { $set: { given_mark: given_mark, type: "MARKED" } }
+      );
+      res.send({ message: "Document updated successfully" });
+    });
+
+    // AUTH RELETED API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.cookie('token', token,{
+        httpOnly: true,
+        secure:true,
+        sameSite:"none"
+      })
+      .send({ success: true});
+    });
+    // user logout hoile jwt
+    app.post('/logout',async(req,res)=>{
+        const user=req.body;
+       console.log("Logging Out", user);
+        res.clearCookie('token',{maxAge:0}).send({success: true});
+    })
 
 
-  app.get("/mylist/:email", async (req, res) => {
-    const  email  = req.params.email;
-    const query={email}
-    const results = await mylistCollection.find(query).toArray();
-    const assignmentList = results?.map((result)=>ObjectId.createFromHexString(result.assignment_id.toString()))
-    const assignments = await assignmentCollection.find({_id:{$in:assignmentList }}).toArray()
-    res.send(assignments)
-  });
-// add all list 
-  app.get("/all-list", async (req, res) => {
 
-    const results = await assignmentCollection.find().toArray()
-    res.send(results)
-  });
-// give mark condition
-  app.put("/give-mark", async (req, res) => {
-    const { assignment_id, given_mark } = req.body;
-    const result = await assignmentCollection.updateOne(
-      { _id: ObjectId.createFromHexString(assignment_id) },
-      { $set: {"given_mark":given_mark,type: "MARKED"} }  
-  );
-  res.send({'message':'Document updated successfully'});
+    // Route to fetch user's submissions by email
+    // app.get('/mylist/:email', (req, res) => {
+    //   const { email } = req.params;
+    //   // Filter submissions by email
+    //   const userSubmissions = submissions.filter(submission => submission.email === email);
+    //   res.status(200).json(userSubmissions);
+    // });
+    // app.get("/mylist/:email", async (req, res) => {
+    //   const  email  = req.params.email;
+    //   const query={email}
+    //   const result = await assignmentCollection.findOne(query).toArray();
+    //   res.send(result);
+    // });
 
-  })
-
-// Route to fetch user's submissions by email
-// app.get('/mylist/:email', (req, res) => {
-//   const { email } = req.params;
-//   // Filter submissions by email
-//   const userSubmissions = submissions.filter(submission => submission.email === email);
-//   res.status(200).json(userSubmissions);
-// });
-  // app.get("/mylist/:email", async (req, res) => {
-  //   const  email  = req.params.email;
-  //   const query={email}
-  //   const result = await assignmentCollection.findOne(query).toArray();
-  //   res.send(result);
-  // });
- 
- 
-  // list all pending
-  //   app.get("/bidrequest/:email", async (req, res) => {
-  //   const  email  = req.params.email;
-  //   const query={email:email}
-  //   const result = await assignmentCollection.findOne(query).toArray();
-  //   res.send(result);
-  // });
-    
-
-  
+    // list all pending
+    //   app.get("/bidrequest/:email", async (req, res) => {
+    //   const  email  = req.params.email;
+    //   const query={email:email}
+    //   const result = await assignmentCollection.findOne(query).toArray();
+    //   res.send(result);
+    // });
 
     app.get("/", (req, res) => {
       res.send("study er Server chole!");
     });
-    
+
     // Ping MongoDB deployment
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -200,15 +232,16 @@ app.post("/mylist", async (req, res) => {
   }
 }
 
-run().
-then(()=>{
-  app.listen(port, () => {
-    console.log(`Server is running on port :${port}`);
+run()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port :${port}`);
+    });
+  })
+  .catch(() => {
+    console.dir;
   });
-})
-.catch(()=>{console.dir});
 
 // Default route
 
 // Start server
-
